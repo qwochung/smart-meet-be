d0e3321dcdffddf06adf1befcefaa7bc9f98c076
qwochung <tranquochung0404@gmail.com>
2026-02-06 15:28:47 +0700

    first commit

---
+++ src/main/java/com/example/smartmeetbe/SmartMeetBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartmeetbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMeetBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMeetBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smartmeetbe/SmartMeetBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartmeetbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMeetBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
